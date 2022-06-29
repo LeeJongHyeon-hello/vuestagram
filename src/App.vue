f2e3ed36e4732fd4b8b5824d3d1899e036f26032
LeejongHyeon <rmaskdkfhk@gmail.com>
2022-06-29 22:50:20 +0900

    axios add

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <Container :postData="postData"/>
+  <button v-on:click="more">더보기</button>
+
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from './components/Container.vue'
+import postData from './assets/PostData.js'
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      postData : postData,
+      moreNum : 0,
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Container,
+  },
+  methods:{
+    more(){ 
+      axios.get(`https://codingapple1.github.io/vue/more${this.moreNum}.json`)
+      .then( result =>{
+        this.postData.push(result.data);
+        this.moreNum++;
+      })
+      .catch(err => {
+        console.log(errrrrrrrrrrrrrrrrr)
+      })
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style.css';
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :postData="postData[i]" v-for="(item,i) in postData" :key="i"></Post>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+    name : 'Container',
+    components : {
+      Post,
+    },
+    props :{
+    postData : Array,
+  },
+};
+</script>
+
+<style>
+</style>
